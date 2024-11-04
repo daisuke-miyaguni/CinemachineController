36a5fbd230d0299e4506da75a1f3a8b5010d0cfe
daisuke.miyaguni <daisuke.miyaguni.0225@gmail.com>
2024-11-05 00:08:01 +0900

    UnityProjectの追加

---
+++ Assets/_Dev/Scripts/Editor/VirtualCameraControllerEditor.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(VirtualCameraController))]
+public class VirtualCameraControllerEditor : Editor
+{
+	static bool BaseGUI = true;
+	bool LayerListOpen = true;
+
+	public override void OnInspectorGUI()
+	{
+		BaseGUI = EditorGUILayout.Foldout(BaseGUI, "BaseGUI");
+		if (BaseGUI)
+		{
+			base.OnInspectorGUI();
+			return;
+		}
+		else
+		{
+			EditorGUI.BeginDisabledGroup(true);
+			EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour((MonoBehaviour)target), typeof(MonoScript), false);
+			EditorGUI.EndDisabledGroup();
+
+			serializedObject.Update();
+			GUIStyle myStyle = new GUIStyle();
+			myStyle.alignment = TextAnchor.MiddleCenter;
+			myStyle.normal.textColor = new Color(0.8f, 0.8f, 0.8f);
+
+			VirtualCameraController vCamController = target as VirtualCameraController;
+			if (vCamController.virtualCameras.Length != vCamController.transform.childCount)
+			{
+				vCamController.AutoVcamsRegister();
+				vCamController.RenderLayerResize();
+			}
+
+			EditorGUILayout.BeginHorizontal();
+			if (GUILayout.Button("<", GUILayout.Width((EditorGUIUtility.currentViewWidth / 5) - 15), GUILayout.Height(20)))
+			{
+				vCamController.SetPrevVirtualCamera();
+				EditorUtility.SetDirty(vCamController);
+			}
+			if (GUILayout.Button("VCam Update", GUILayout.Width(EditorGUIUtility.currentViewWidth * 3 / 5), GUILayout.Height(20)))
+			{
+				vCamController.AutoVcamsRegister();
+				vCamController.RenderLayerResize();
+				//EditorUtility.SetDirty(vCamController);
+			}
+			if (GUILayout.Button(">", GUILayout.Width((EditorGUIUtility.currentViewWidth / 5) - 15), GUILayout.Height(20)))
+			{
+				vCamController.SetNextVirtualCamera();
+				EditorUtility.SetDirty(vCamController);
+			}
+			EditorGUILayout.EndHorizontal();
+			EditorGUILayout.Separator();
+
+
+			EditorGUILayout.LabelField("Virtual Cameras", myStyle);
+			var vCams = vCamController.virtualCameras;
+
+			EditorGUILayout.BeginHorizontal();
+			GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+			buttonStyle.wordWrap = true;
+
+			var color = GUI.color;
+
+			for (int index = 0; index < vCams.Length; index++)
+			{
+				if (index == vCamController.GetCurrentVcamIndex())
+				{
+					GUI.color = new Color(1f, 0f, 0f);
+				}
+				else
+				{
+					GUI.color = color;
+				}
+
+				if (index > 4 && index % 5 == 0)
+				{
+					EditorGUILayout.EndHorizontal();
+					EditorGUILayout.BeginHorizontal();
+				}
+
+				if (GUILayout.Button(vCams[index].Name, buttonStyle, GUILayout.MinWidth(80), GUILayout.Height(80)))
+				{
+					vCamController.SetVcam(index);
+
+				}
+			}
+			EditorGUILayout.EndHorizontal();
+			EditorUtility.SetDirty(vCamController);
+			GUI.color = color;
+
+
+			EditorGUILayout.Separator();
+			var VCamLayer = vCamController.vCamRenderLayer;
+			LayerListOpen = EditorGUILayout.Foldout(LayerListOpen, "Camera CullingMask");
+
+			if (LayerListOpen)
+			{
+				for (int index = 0; index < VCamLayer.Length; index++)
+				{
+					VCamLayer[index] = LayerEditorGUILayout.LayerMaskField(string.Format("{0} CameraLayer", vCams[index].Name), VCamLayer[index]);
+					EditorUtility.SetDirty(vCamController);
+				}
+			}
+
+			EditorGUILayout.Separator();
+			EditorGUI.BeginChangeCheck();
+			vCamController.useGamePadController = EditorGUILayout.Toggle("Use GamePad ControllCamera", vCamController.useGamePadController);
+			if (vCamController.useGamePadController)
+			{
+				vCamController.inputKey = EditorGUILayout.TextField("input key", vCamController.inputKey);
+				vCamController.OnThreashold = EditorGUILayout.FloatField("OnThreashold", vCamController.OnThreashold);
+				vCamController.OffThreashold = EditorGUILayout.FloatField("OffThreashold", vCamController.OffThreashold);
+			}
+			vCamController.useNumPad = EditorGUILayout.Toggle("Use NumPad ControllCamera", vCamController.useNumPad);
+			if (vCamController.useNumPad)
+			{
+				for (int index = 0; index < 10; index++)
+				{
+					vCamController.Kaypad[index] = (KeyCode)EditorGUILayout.EnumPopup("KeyPad", vCamController.Kaypad[index]);
+				}
+			}
+			if (EditorGUI.EndChangeCheck())
+			{
+				EditorUtility.SetDirty(vCamController);
+			}
+			// base.OnInspectorGUI();
+		}
+	}
+
+}
+
+public static class LayerEditorGUILayout
+{
+
+	// LayerMaskFieldはデフォルトでは無いので自作.
+	public static LayerMask LayerMaskField(
+		string label,
+		LayerMask layerMask)
+	{
+		List<string> layers = new List<string>();
+		List<int> layerNumbers = new List<int>();
+
+		for (var i = 0; i < 32; ++i)
+		{
+			string layerName = LayerMask.LayerToName(i);
+			if (!string.IsNullOrEmpty(layerName))
+			{
+				layers.Add(layerName);
+				layerNumbers.Add(i);
+			}
+		}
+
+		int maskWithoutEmpty = 0;
+		for (var i = 0; i < layerNumbers.Count; ++i)
+		{
+			if (0 < ((1 << layerNumbers[i]) & layerMask.value))
+				maskWithoutEmpty |= 1 << i;
+		}
+
+		maskWithoutEmpty = EditorGUILayout.MaskField(label, maskWithoutEmpty, layers.ToArray());
+		int mask = 0;
+		for (var i = 0; i < layerNumbers.Count; ++i)
+		{
+			if (0 < (maskWithoutEmpty & (1 << i)))
+				mask |= 1 << layerNumbers[i];
+		}
+		layerMask.value = mask;
+
+		return layerMask;
+	}
+}
---
+++ Assets/_Dev/Scripts/VirtualCameraController.cs
@@ -0,0 +1,287 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public sealed class VirtualCameraController : MonoBehaviour
+{
+	public CinemachineVirtualCamera[] virtualCameras = new CinemachineVirtualCamera[] { };
+	private int currentCameraIndex = 0;
+
+	[SerializeField] public LayerMask[] vCamRenderLayer = new LayerMask[] { };
+
+	#region カメラ同期用変数
+	private int SyncCameraIndex = 0;
+	public int SyncCameraIndexProperty
+	{
+		get { return SyncCameraIndex; }
+		set { SyncCameraIndex = value; }
+	}
+	#endregion
+
+	#region InputSystemでカメラをスイッチさせる機能
+
+	[SerializeField] public bool useGamePadController = true;
+	[SerializeField] public string inputKey = "D_Pad_Vertical";
+	[SerializeField] public float OnThreashold = 0.9f;
+	[SerializeField] public float OffThreashold = 0.25f;
+
+	#endregion GamePadでカメラをスイッチさせる機能
+
+	#region テンキーでカメラをスイッチさせる機能
+	[SerializeField] public bool useNumPad = false;
+
+	[SerializeField]
+	public KeyCode[] Kaypad =
+	{
+		KeyCode.Keypad1,
+		KeyCode.Keypad2,
+		KeyCode.Keypad3,
+		KeyCode.Keypad4,
+		KeyCode.Keypad5,
+		KeyCode.Keypad6,
+		KeyCode.Keypad7,
+		KeyCode.Keypad8,
+		KeyCode.Keypad9,
+		KeyCode.Keypad0,
+	};
+
+	private void CameraSwichKeypad()
+	{
+		if (!useNumPad) { return; }
+		for (int i = 0; i < Kaypad.Length; i++)
+		{
+			if (Input.GetKeyDown(Kaypad[i]))
+			{
+				SetVcam(i);
+			}
+		}
+	}
+	#endregion テンキーでカメラをスイッチさせる機能
+
+	private void Reset()
+	{
+		AutoVcamsRegister();
+		RenderLayerResize();
+	}
+
+	// Inspector更新CallBack
+	public void OnValidate()
+	{
+		AutoVcamsRegister();
+		RenderLayerResize();
+	}
+
+	// VirtualCameraとVCamRenderLayerの数が違ったら同じにする
+	public void RenderLayerResize()
+	{
+		while (virtualCameras.Length != vCamRenderLayer.Length)
+		{
+			if (virtualCameras.Length > vCamRenderLayer.Length)
+			{
+				System.Array.Resize(ref vCamRenderLayer, vCamRenderLayer.Length + 1);
+				vCamRenderLayer[vCamRenderLayer.Length - 1] = ~0;
+			}
+			else if (virtualCameras.Length < vCamRenderLayer.Length)
+			{
+				System.Array.Resize(ref vCamRenderLayer, vCamRenderLayer.Length - 1);
+			}
+		}
+	}
+
+	void Start()
+	{
+		// Vcamの子供を取得して配列に保存
+		AutoVcamsRegister();
+
+		// 開始時のカメラIndexを取得
+		currentCameraIndex = GetCurrentVcamIndex();
+
+		// 開始時にLayerMaskの設定が漏れていた場合に自動で設定する
+		if (vCamRenderLayer.Length != virtualCameras.Length)
+		{
+			if (vCamRenderLayer != null)
+			{
+				LayerMask[] initLayerMask = vCamRenderLayer;
+				vCamRenderLayer = new LayerMask[virtualCameras.Length];
+
+				for (int i = 0; i < initLayerMask.Length; i++)
+				{
+					vCamRenderLayer[i] = initLayerMask[i];
+				}
+
+				for (int j = initLayerMask.Length; j < vCamRenderLayer.Length; j++)
+				{
+					vCamRenderLayer[j] = Camera.main.cullingMask;
+				}
+			}
+			else
+			{
+				vCamRenderLayer = new LayerMask[virtualCameras.Length];
+				for (int i = 0; i < vCamRenderLayer.Length; i++)
+				{
+					vCamRenderLayer[i] = Camera.main.cullingMask;
+				}
+			}
+		}
+		// カメラのマスクを設定する
+		Camera.main.cullingMask = vCamRenderLayer[currentCameraIndex];
+
+#if UNITY_EDITOR
+		SceneView.duringSceneGui += OnSceneView;
+#endif
+	}
+
+	private void Update()
+	{
+		CameraSwichKeypad();
+	}
+
+	/// <summary>
+	/// Cameraを次のカメラに切り変える。
+	/// Camera は virtualCameras の範囲でループさせる 
+	/// </summary>
+	[ContextMenu("Next Vcams")]
+	public void SetNextVirtualCamera()
+	{
+		// 現在のカメラのIndexを取得
+		int currentIndex = GetCurrentVcamIndex();
+
+		// CameraのIndexをインクリメントして範囲外なら0に戻す
+		currentIndex++;
+		if (currentIndex >= virtualCameras.Length) { currentIndex -= virtualCameras.Length; }
+		SetVcam(currentIndex);
+	}
+	[ContextMenu("Prev Vcams")]
+	public void SetPrevVirtualCamera()
+	{
+		// 現在のカメラのIndexを取得
+		int currentIndex = GetCurrentVcamIndex();
+
+		currentIndex--;
+		if (currentIndex < 0) { currentIndex += virtualCameras.Length; }
+		SetVcam(currentIndex);
+	}
+
+	public bool SetVcam(int cameraIndex)
+	{
+		SyncCameraIndex = cameraIndex;
+		LayerMask num = LayerMask.GetMask(new string[] { LayerMask.LayerToName(15), LayerMask.LayerToName(16), LayerMask.LayerToName(17), });
+
+		const int HighPriority = 10;
+		const int LowPriority = 0;
+
+		currentCameraIndex = cameraIndex;
+
+		if (cameraIndex >= virtualCameras.Length) { return false; }
+
+		// priority のリセット
+		for (int i = 0; i < virtualCameras.Length; i++)
+		{
+			virtualCameras[i].Priority = LowPriority;
+		}
+
+		// priority の再セット
+		virtualCameras[cameraIndex].Priority = HighPriority;
+		Camera.main.cullingMask = vCamRenderLayer[cameraIndex];
+
+		return true;
+	}
+
+	public bool SetVcam()
+	{
+		LayerMask num = LayerMask.GetMask(new string[] { LayerMask.LayerToName(15), LayerMask.LayerToName(16), LayerMask.LayerToName(17), });
+
+		const int HighPriority = 10;
+		const int LowPriority = 0;
+
+		currentCameraIndex = SyncCameraIndex;
+
+		if (SyncCameraIndex >= virtualCameras.Length) { return false; }
+
+		// priority のリセット
+		for (int i = 0; i < virtualCameras.Length; i++)
+		{
+			virtualCameras[i].Priority = LowPriority;
+		}
+
+		// priority の再セット
+		virtualCameras[SyncCameraIndex].Priority = HighPriority;
+		Camera.main.cullingMask = vCamRenderLayer[SyncCameraIndex];
+
+		return true;
+	}
+
+	public int GetCurrentVcamIndex()
+	{
+		int maxPriority = int.MinValue;
+		int resultIndex = int.MaxValue;
+
+		for (int i = 0; i < virtualCameras.Length; i++)
+		{
+			int currentPriority = virtualCameras[i].Priority;
+			if (maxPriority < currentPriority)
+			{
+				maxPriority = currentPriority;
+				resultIndex = i;
+			}
+		}
+
+		if (maxPriority == int.MinValue) { Debug.LogError("Get VirtualCamera Index Error!"); }
+		return resultIndex;
+	}
+
+	[ContextMenu("Auto Vcams Register")]
+	public void AutoVcamsRegister()
+	{
+		virtualCameras = transform.GetComponentsInChildren<CinemachineVirtualCamera>();
+	}
+
+
+#if UNITY_EDITOR
+	void OnDestroy()
+	{
+		SceneView.duringSceneGui -= OnSceneView;
+	}
+
+
+	void OnSceneView(SceneView sceneView)
+	{
+		const float RecButtonWidth = 70f;
+		Handles.BeginGUI();
+
+		GUILayout.BeginArea(new Rect(0, 0
+			, Screen.width / EditorGUIUtility.pixelsPerPoint
+			, Screen.height / EditorGUIUtility.pixelsPerPoint));
+
+		for (int index = 0; index < virtualCameras.Length; index++)
+		{
+			if (index == currentCameraIndex)
+			{
+				// 現在使用中のカメラならラベル。
+				GUILayout.Label(virtualCameras[index].name, GUILayout.Width(RecButtonWidth));
+			}
+			else
+			{
+				// 使っていないもカメラは切り替えができるようボタンで配置する
+				bool isPush = GUILayout.Button(virtualCameras[index].name, GUILayout.Width(RecButtonWidth));
+
+				// ボタンが押されたら切り替える
+				if (isPush)
+				{
+					// Buttonが押されたらそのカメラに切り替える
+					SetVcam(index);
+				}
+			}
+		}
+
+
+		GUILayout.EndArea();
+
+		Handles.EndGUI();
+	}
+#endif
+}
